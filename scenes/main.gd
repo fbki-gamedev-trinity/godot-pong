f7a3bc63b338e6c5589d54642e18c2ffc495e48b
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-10-19 12:41:40 +0800

    Endgame WIP
    - Add score objective spinbox
    - Add score checks

--- scenes/main.gd
+++ scenes/main.gd
@@ -11,13 +11,21 @@ var enemy_points = 0
 func player_scored() -> void:
 	player_points += 1
 	UI.update_player_point(player_points)
-	reset_game_state()
+	test_win_objective()
 
 func enemy_scored() -> void:
 	enemy_points += 1
 	UI.update_enemy_points(enemy_points)
-	reset_game_state()
-	
+	test_win_objective()
+
+func test_win_objective():
+	if player_points >= Global.OBJECTIVE:
+		pass
+	elif enemy_points >= Global.OBJECTIVE:
+		pass
+	else:
+		reset_game_state()
+
 func reset_game_state():
 	enemy_paddle.global_position.y = 0
 	player_paddle.global_position.y = 0
@@ -26,3 +34,4 @@ func reset_game_state():
 	player_paddle.linear_velocity = Vector2.ZERO
 	ball.global_position = Vector2.ZERO
 	ball.start_ball()
+	
--- scenes/menu.gd
+++ scenes/menu.gd
@@ -1,11 +1,12 @@
 extends Control
 
-
-func _on_one_player_pressed() -> void:
+func transition_to_game_scene(type: int):
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 	Global.TYPE = 0
+	Global.OBJECTIVE = $MarginContainer/VBoxContainer/PlayUntil.value
 
+func _on_one_player_pressed() -> void:
+	transition_to_game_scene(0)
 
 func _on_two_players_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
-	Global.TYPE = 1
+	transition_to_game_scene(1)
